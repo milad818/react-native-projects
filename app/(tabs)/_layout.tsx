73f11338f351a6ec756540397208ad2d7dcdf295
Milad Zakhireh <eng.zakhireh@gmail.com>
2026-01-20 22:26:18 +0100

    Add icon to Home tab importing vector-icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,22 @@
 import { Tabs } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Fontisto from '@expo/vector-icons/Fontisto';
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
+    <Tabs screenOptions={{tabBarActiveTintColor: "green",
+                          tabBarActiveBackgroundColor: "lightgray"}}>
+      
+      <Tabs.Screen name="index"
+                   options={{ title: "Home",
+                              tabBarIcon: ({color, focused}) => {
+                              return focused ?
+                                (<Fontisto name="home" size={22} color={color} />) :
+                                (<AntDesign name="home" size={24} color="black" />)
+                              }
+                                
+                            }} />
       <Tabs.Screen name="login" options={{ title: "Login" }} />
     </Tabs>
   );
