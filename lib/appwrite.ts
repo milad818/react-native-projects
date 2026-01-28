575f9878c2ae47d85e5d1f384ab2739737329aa6
Milad Zakhireh <eng.zakhireh@gmail.com>
2026-01-28 21:33:08 +0100

    Complete adding a habit functionality so that the entry/habit is inserted into the database successfully

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -10,7 +10,7 @@ export const client = new Client().setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_
 
 // Specify what services to initialize in the project for appwrite
 export const account = new Account(client);
-export const database = new Databases(client);
+export const databases = new Databases(client);
 
-const DATABASE_ID = process.env.EXPO_PUBLIC_DB_ID!
-const HABITS_COLLECTION_ID = process.env.EXPO_PUBLIC_HABITS_COLLECTION_ID
+export const DATABASE_ID = process.env.EXPO_PUBLIC_DB_ID!
+export const HABITS_COLLECTION_ID = process.env.EXPO_PUBLIC_HABITS_COLLECTION_ID!
