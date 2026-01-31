a8707e85f485c7bc91ef5205752b7ea57e12da24
Milad Zakhireh <eng.zakhireh@gmail.com>
2026-01-31 12:03:43 +0100

    - Implement habit retrieval from the corresponding table in the database - Implement habit subscription to subscribe to realtime events -  Add scroll view to habits' screen and style the screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,143 @@
+import { DATABASE_ID, databases, HABITS_COLLECTION_ID, client, RealtimeResponse } from "@/lib/appwrite";
 import { useAuth } from "@/lib/auth-context";
+import { Habit } from "@/types/database.type";
+import MaterialCommunityIcons from '@expo/vector-icons/AntDesign';
 import { Link } from "expo-router";
-import { Text, View, StyleSheet } from "react-native";
-import { Button } from "react-native-paper";
+import { useEffect, useState } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
+import { Query } from "react-native-appwrite";
+import { Button, Card, Surface, Text } from "react-native-paper";
+import { red } from "react-native-reanimated/lib/typescript/Colors";
 
 export default function Index() {
 
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+
+  // NOTE! The state defined below is a list of Habit objects
+  const [habits, setHabits] = useState<Habit[]>();
+
+  // Fetch habits every time the user navigates to this screen (Today's Habits)
+  // And it will reload only if the user changes passing the user to the dependency array
+  useEffect(() => {
+    if (user) {
+      // Build the realtime channel string for listening to document changes
+      // in the Habits collection of the specified database
+      const channel = `databases.${DATABASE_ID}.collections.${HABITS_COLLECTION_ID}.documents`
+
+      // Subscribe to realtime events on the Habits collection
+      const habitsSubscription = client.subscribe(
+        channel,
+        (response: RealtimeResponse) => {
+
+          // Check which type of event was triggered
+          const isCreate = response.events.includes("databases.*.collections.*.documents.*.create")
+          const isUpdate = response.events.includes("databases.*.collections.*.documents.*.update")
+          const isDelete = response.events.includes("databases.*.collections.*.documents.*.delete")
+
+          // If a habit was created, updated, or deleted,
+          // refetch the habits to keep UI state in sync
+          if (isCreate) {
+            fetchHabits();
+          } else if (isUpdate) {
+            fetchHabits();
+          } else if (isDelete) {
+            fetchHabits();
+          }
+        }
+      )
+
+      // Initial fetch when the component mounts
+      // or when the `user` dependency changes
+      fetchHabits();
+
+      // Cleanup function:
+      // unsubscribe from realtime updates when the component unmounts
+      // or before re-running the effect
+      return () => {
+        habitsSubscription();
+      };
+    }
+  }, [user]) // Re-run effect when the user changes. [Why wouldn't add habits to dependency array to fetch the most recent?]
+
+  const fetchHabits = async () => {
+    try {
+      // Adding <Habit> we explicitly tell TypeScript that these documents follow the Habit shape
+      const response = await databases.listDocuments<Habit>(
+        DATABASE_ID,
+        HABITS_COLLECTION_ID,
+        [Query.equal("user_id", user?.$id ?? "")]
+      )
+
+      // Investigate document content
+      // console.log("Retrieved Appwrite documents: \n", response.documents)
+      // console.log("Retrieved Appwrite documents: \n", response.documents[0])
+      // console.log("Retrieved documents length: \n", response.documents.length)
+
+      setHabits(response.documents as Habit[]);
+
+      // NOTE! 
+      // DefaultDocument[] does not sufficiently overlap with Habit[]
+      // AND TypeScript cannot verify that fields like title, frequency, etc. actually exist
+
+      // If the type of response not set to Habit you can set Habits as below      
+      // setHabits(
+      //   response.documents.map(doc => ({
+      //     ...doc,
+      //     user_id: doc.user_id,
+      //     title: doc.title,
+      //     description: doc.description,
+      //     streak_count: doc.streak_count,
+      //     last_completed: doc.last_completed,
+      //     frequency: doc.frequency
+      //   })) as Habit[]);
+
+      // OR
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello and welcome to Habit Tracker project!</Text>
-      <Button mode="text" onPress={signOut} icon={"logout"}>
-        Sign Out
-      </Button>
+      <View style={styles.header}>
+        <Text style={styles.title} variant="headlineSmall">Today&apos;s Habit</Text>
+        <Button mode="text" onPress={signOut} icon={"logout"}>
+          Sign Out
+        </Button>
+      </View>
+      <ScrollView>
+        {habits?.length === 0 ? (
+          <View style={styles.emptyState} >
+            <Text style={styles.emptyStateText}>No habits yet. Add your first habit!</Text>
+          </View>
+        ) : (
+          // "?" below/above prevents a crash if habits is still undefined
+          // unless initialized with and empty array []
+          habits?.map((habit, key) => (
+            <Surface style={styles.card} key={key} elevation={2}>
+              <View style={styles.cardContent}>
+                <Text style={styles.cardTitle}> {habit.title} </Text>
+                <Text style={styles.cardDescription}> {habit.description} </Text>
+                <View style={styles.cardFooter}>
+                  <View style={styles.streakBadge}>
+                    <MaterialCommunityIcons
+                      name="fire"
+                      size={18}
+                      color={"#ff8d23"} />
+                    <Text style={styles.streakText}>
+                      {habit.streak_count} day streak
+                    </Text>
+                  </View>
+                  <View style={styles.frequencyBadge}>
+                    <Text style={styles.frequencyText}> {habit.frequency.charAt(0).toUpperCase() + habit.frequency.slice(1)}</Text>
+                  </View>
+                </View>
+              </View>
+            </Surface>
+          ))
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -21,20 +146,96 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    padding: 16,
+    backgroundColor: "#f5f5f5",
+  },
+
+  header: {
+    color: "brown",
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    backgroundColor: "#fffddf"
+    marginBottom: 24
   },
 
-  text: {
-    color: "brown"
+  title: {
+    fontWeight: "bold"
   },
 
-  navButton: {
-    width: 60,
-    height: 20,
-    backgroundColor: "coral",
+  emptyState: {
+
+  },
+
+  emptyStateText: {
+
+  },
+
+  card: {
+    marginBottom: 18,
     borderRadius: 5,
-    textAlign: "center"
+    backgroundColor: "#e1ecff",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.08,
+    shadowRadius: 8,
+  },
+
+  cardContent: {
+    padding: 10
+  },
+
+  cardTitle: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: "#303071"
+  },
+
+  cardDescription: {
+    fontSize: 15,
+    marginBottom: 15,
+    color: "#58585e"
+  },
+
+  cardFooter: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+
+  streakBadge: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#ffd65c",
+    borderRadius: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 3
+  },
+
+  streakText: {
+    marginLeft: 5,
+    marginRight: 5,
+    color: "#e94904",
+    fontWeight: "bold",
+    fontSize: 13
+  },
+
+  frequencyBadge: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#62fe77",
+    borderRadius: 10,
+    paddingHorizontal: 6,
+    paddingVertical: 3
+  },
+
+  frequencyText: {
+    marginLeft: 3,
+    marginRight: 3,
+    color: "#e86d39",
+    fontWeight: "bold",
+    fontSize: 13,
+    // textTransform: "uppercase"
   }
+
 })
---
+++ types/database.type.ts
@@ -0,0 +1,13 @@
+import { Models } from "react-native-appwrite";
+
+
+// Habit is extended so it will also include the specific attributes of an Appwrite document
+export interface Habit extends Models.Document {
+  user_id: string;
+  title: string;
+  description: string;
+  streak_count: number;
+  last_completed: string;
+  frequency: string;
+  // created_at: string;
+}
